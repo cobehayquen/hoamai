ac726ca89a9ac8593740cd032cbfff1445be20de
cobehayquen <124519215+cobehayquen@users.noreply.github.com>
2023-02-12 23:05:12 +0700

    Add files via upload

---
+++ src/Entry.java
@@ -0,0 +1,9 @@
+public class Entry {
+	public static void main(String[] args) {
+		Student s = new Student("Hoang", 19, "Dong Hoi", 5.0);
+		s.display();
+		System.out.println();
+		Teacher t = new Teacher("Thi", 25, "Sai Gon", 77700);
+		t.display();
+	}
+}
